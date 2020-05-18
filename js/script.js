63c757d3672e1256f7652ee67720a56fded2c11a
prashathc <prashathc@gmail.com>
2020-05-18 16:30:07 -0400

    changing logo size

--- js/script.js
+++ js/script.js
@@ -2,22 +2,22 @@
 var scroll = new SmoothScroll('a[href*="#"]', {
   speed: 500,
   speedAsDuration: true,
-  offset: 75,
+  offset: 95,
 });
 
 // Mobile navbar
 
-const toggleButton = document.getElementsByClassName('toggle-button')[0]
-const navbarLinks = document.getElementsByClassName('menu-items')[0]
+const toggleButton = document.getElementsByClassName("toggle-button")[0];
+const navbarLinks = document.getElementsByClassName("menu-items")[0];
 
+toggleButton.addEventListener("click", () => {
+  navbarLinks.classList.toggle("active");
+});
 
-toggleButton.addEventListener('click', () => {
-  navbarLinks.classList.toggle('active')
-})
+const menuItems = Array.from(document.querySelectorAll(".menu-items a"));
 
-
-const menuItems = Array.from(document.querySelectorAll('.menu-items a'))
-
-menuItems.forEach(item => item.addEventListener('click', () => {
-  navbarLinks.classList.toggle('active')
-}))
+menuItems.forEach((item) =>
+  item.addEventListener("click", () => {
+    navbarLinks.classList.toggle("active");
+  })
+);
